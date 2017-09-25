8200a13fb6a924354440cb3f082758fbec28ca5b
Steven <onlinecop@gmail.com>
2017-09-24 21:00:06 -0400

    Moved property comments to line above property, instead of on the same line but indnted some arbitrary amount to align with comments nearby.

--- include/fighter.h
+++ include/fighter.h
@@ -10,38 +10,94 @@ class Raster;
  * See enemy_init() for more information on the fields.
  */
 typedef struct {
-	char name[25];          /** Name */
-	int xp;                 /** Experience Points */
-	int gp;                 /** Gold Points */
-	int lvl;                /** Level */
-	int cx;                 /** x-coord of image in datafile */
-	int cy;                 /** y-coord of image in datafile */
-	int cw;                 /** width in datafile */
-	int cl;                 /** height in datafile */
-	int hp;                 /** Hit Points */
-	int mhp;                /** Max Hit Points */
-	int mp;                 /** Magic Points */
-	int mmp;                /** Max Magic Points */
-	int dip;                /** Defeat Item Probability: Probability in % that the enemy will yield an item when defeated. */
-	int defeat_item_common; /** Defeat Item Common: If the enemy yields an item, you will get this item 95% of the time. */
-	int defeat_item_rare;   /** Defeat Item Rare: If the enemy yields an item, you will get this item 5% of the time. */
-	int steal_item_common;  /** Steal Item Common: If Ayla steals something, she will get this item 95% of the time. */
-	int steal_item_rare;    /** Steal Item Rare: If Ayla steals something, she will get this item 5% of the time. */
-	int stats[NUM_STATS];   /** See A_* constants in kq.h */
-	char res[NUM_RES];      /** eResistance: See R_* constants */
-	uint8_t facing;         /** Direction character's sprite faces */
-	uint8_t aframe;         /** Battle sprite to display (standing, casting, attacking) */
+	/** Name */
+	char name[25];
+
+	/** Experience Points */
+	int xp;
+
+	/** Gold Points */
+	int gp;
+
+	/** Level */
+	int lvl;
+
+	/** x-coord of image in datafile */
+	int cx;
+
+	/** y-coord of image in datafile */
+	int cy;
+
+	/** width in datafile */
+	int cw;
+
+	/** height in datafile */
+	int cl;
+
+	/** Hit Points */
+	int hp;
+
+	/** Max Hit Points */
+	int mhp;
+
+	/** Magic Points */
+	int mp;
+
+	/** Max Magic Points */
+	int mmp;
+
+	/** Defeat Item Probability: Probability in % that the enemy will yield an item when defeated. */
+	int dip;
+
+	/** Defeat Item Common: If the enemy yields an item, you will get this item 95% of the time. */
+	int defeat_item_common;
+
+	/** Defeat Item Rare: If the enemy yields an item, you will get this item 5% of the time. */
+	int defeat_item_rare;
+
+	/** Steal Item Common: If Ayla steals something, she will get this item 95% of the time. */
+	int steal_item_common;
+
+	/** Steal Item Rare: If Ayla steals something, she will get this item 5% of the time. */
+	int steal_item_rare;
+
+	/** See A_* constants in kq.h */
+	int stats[NUM_STATS];
+
+	/** eResistance: See R_* constants */
+	char res[NUM_RES];
+
+	/** Direction character's sprite faces */
+	uint8_t facing;
+
+	/** Battle sprite to display (standing, casting, attacking) */
+	uint8_t aframe;
+
 	uint8_t crit;
-	uint8_t sts[NUM_SPELLTYPES]; /** eSpellType */
+
+	/** eSpellType */
+	uint8_t sts[NUM_SPELLTYPES];
+
 	uint8_t defend;
 	uint8_t ai[8];
 	uint8_t aip[8];
 	uint8_t atrack[8];
-	uint32_t csmem;         /** Spell number, associated with M_* spells, used within s_spell magic[] array. */
-	int ctmem;              /** Spell target: who is going to be affected by the spell; can be set to -1 */
-	uint32_t current_weapon_type; /** Current Weapon Type: The shape of the currently held weapon (sword, dagger, axe etc) \sa hero_init() */
-	int welem;              /** eResistance: Which Element type (sick, fire, water, etc.) */
-	int unl;                /** UNLiving (undead), like zombies, skeletons, etc. */
+
+	/** Spell number, associated with M_* spells, used within s_spell magic[] array. */
+	uint32_t csmem;
+
+	/** Spell target: who is going to be affected by the spell; can be set to -1 */
+	int ctmem;
+
+	/** Current Weapon Type: The shape of the currently held weapon (sword, dagger, axe etc) \sa hero_init() */
+	uint32_t current_weapon_type;
+
+	/** eResistance: Which Element type (sick, fire, water, etc.) */
+	int welem;
+
+	/** UNLiving (undead), like zombies, skeletons, etc. */
+	int unl;
+
 	int aux;
 	int bonus;
 	int bstat;
